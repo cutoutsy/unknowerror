498a119337ec2b4931d115f9d816b15519c49f0c
cutoutsy <cutoutsy@gmail.com>
2017-06-07 09:37:17 +0800

    add PostType entity config

--- src/main/java/entity/Post.java
+++ src/main/java/entity/Post.java
@@ -29,4 +29,180 @@ public class Post {
     private int favoriteCount;
     private Timestamp closedDate;
     private Timestamp communityOwnedDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPostTypeId() {
+        return postTypeId;
+    }
+
+    public void setPostTypeId(int postTypeId) {
+        this.postTypeId = postTypeId;
+    }
+
+    public int getAcceptedAnswerId() {
+        return acceptedAnswerId;
+    }
+
+    public void setAcceptedAnswerId(int acceptedAnswerId) {
+        this.acceptedAnswerId = acceptedAnswerId;
+    }
+
+    public int getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Timestamp getDeletionDate() {
+        return deletionDate;
+    }
+
+    public void setDeletionDate(Timestamp deletionDate) {
+        this.deletionDate = deletionDate;
+    }
+
+    public int getScore() {
+        return Score;
+    }
+
+    public void setScore(int score) {
+        Score = score;
+    }
+
+    public int getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(int viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getOwnerUserId() {
+        return ownerUserId;
+    }
+
+    public void setOwnerUserId(int ownerUserId) {
+        this.ownerUserId = ownerUserId;
+    }
+
+    public String getOwnerDisplayName() {
+        return ownerDisplayName;
+    }
+
+    public void setOwnerDisplayName(String ownerDisplayName) {
+        this.ownerDisplayName = ownerDisplayName;
+    }
+
+    public int getLastEditorUserId() {
+        return lastEditorUserId;
+    }
+
+    public void setLastEditorUserId(int lastEditorUserId) {
+        this.lastEditorUserId = lastEditorUserId;
+    }
+
+    public String getLastEditorDisplayName() {
+        return lastEditorDisplayName;
+    }
+
+    public void setLastEditorDisplayName(String lastEditorDisplayName) {
+        this.lastEditorDisplayName = lastEditorDisplayName;
+    }
+
+    public Timestamp getLastEditDate() {
+        return lastEditDate;
+    }
+
+    public void setLastEditDate(Timestamp lastEditDate) {
+        this.lastEditDate = lastEditDate;
+    }
+
+    public Timestamp getLastActivityDate() {
+        return lastActivityDate;
+    }
+
+    public void setLastActivityDate(Timestamp lastActivityDate) {
+        this.lastActivityDate = lastActivityDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public int getAnswerCount() {
+        return answerCount;
+    }
+
+    public void setAnswerCount(int answerCount) {
+        this.answerCount = answerCount;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(int commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public int getFavoriteCount() {
+        return favoriteCount;
+    }
+
+    public void setFavoriteCount(int favoriteCount) {
+        this.favoriteCount = favoriteCount;
+    }
+
+    public Timestamp getClosedDate() {
+        return closedDate;
+    }
+
+    public void setClosedDate(Timestamp closedDate) {
+        this.closedDate = closedDate;
+    }
+
+    public Timestamp getCommunityOwnedDate() {
+        return communityOwnedDate;
+    }
+
+    public void setCommunityOwnedDate(Timestamp communityOwnedDate) {
+        this.communityOwnedDate = communityOwnedDate;
+    }
 }
--- src/main/java/entity/PostType.java
+++ src/main/java/entity/PostType.java
@@ -6,4 +6,20 @@ package entity;
 public class PostType {
     private int id;
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
